aee5628ea25f71bdc23cf73880125c4a22bbb15f
Jakub Kulhanek <jakub.kulhanek@ness.com>
2017-04-18 21:30:23 +0200

    Version 1.0

--- js/contact_me.js
+++ js/contact_me.js
@@ -11,8 +11,6 @@ $(function() {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -22,9 +20,7 @@ $(function() {
                 url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
-                    name: name,
-                    email: email,
-                    message: message
+                    name: name
                 },
                 cache: false,
                 success: function() {
@@ -33,7 +29,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Děkujeme, Vaše zpráva byla odeslána!. </strong>");
+                        .append("<strong>Skvělé, těšíme se na Vás!</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,7 +41,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Litujeme, ale zprávu se nepodařilo odeslat. Zkuste to prosím později!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Litujeme, ale potvrzení se nepodařilo odeslat. Zkuste to prosím později!"));
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,23 +1,16 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
+if(empty($_POST['name'])) {
    echo "No arguments Provided!";
    return false;
-   }
+}
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = 'kulhanek.kuba@gmail.com';
-$email_subject = "Svatební web od:  $name";
-$email_body = "Jméno: $name\n\nEmail: $email_address\n\Vzkaz:\n\n$message";
-$headers = "From: email@svatba.com\n"; 
+$email_subject = "Svatba - potvrzení účasti";
+$email_body = "<b>$name</b>\n\nPotvrdil účast na svatbě.";
+$headers = "From: email@svatba.com\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
